a0187d586029bc6a2d8c6d899d2fd4c604b1d61c
Kaung Myat Han <arkarhtethan@gmail.com>
2020-03-17 18:42:17 +0630

    todo app finished

---
+++ index.js
@@ -0,0 +1,94 @@
+//jshint esversion:6
+
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const mongoose = require("mongoose");
+
+const app = express();
+
+const items = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://127.0.0.1:27017/todolist", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const itemScheme = {
+    name: String,
+};
+
+const Item = mongoose.model("Item", itemScheme);
+
+const item1 = new Item({
+    name: "Welcome to your todolist!",
+});
+
+
+const item2 = new Item({
+    name: "Hit + to add new item",
+});
+
+const item3 = new Item({
+    name: "<--- Hit this to delete item",
+});
+
+
+app.get("/", function (req, res) {
+    var today = new Date();
+    var option = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+    };
+    var day = today.toLocaleDateString("en-US")
+    Item.find({}, function (error, foundItems) {
+
+        if (foundItems.length === 0) {
+            Item.insertMany([item1, item2, item3], function (error) {
+                if (error) {
+                    console.log("Error occur while adding items");
+                } else {
+                    console.log("Successfully added items");
+
+                    response.redirect("/");
+                }
+            });
+
+        } else {
+            res.render("list", { 'kindOfDay': day, "items": foundItems });
+        }
+    })
+
+});
+
+app.post('/', function (request, response) {
+    var itemName = request.body.newItem;
+    const item = new Item({
+        name: itemName
+    });
+    item.save();
+    response.redirect(
+        "/"
+    );
+})
+
+app.post('/delete', function (request, response) {
+    var itemId = request.body.checkbox;
+    Item.findByIdAndRemove("" + itemId, function (error) {
+        console.log(error);
+        if (!error) {
+            console.log("Success");
+            response.redirect("/");
+        }
+    })
+})
+
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000.");
+});
